740d0cca4c0afdbe5b86139cbc82a9c28ee16f7a
Vitalina <vitalinamazur296@gmail.com>
2024-09-23 21:08:19 +0300

    Labap3.1

--- LabAP3.1/LabAP3.1.cpp
+++ LabAP3.1/LabAP3.1.cpp
@@ -1,39 +1,44 @@
 // Lab_03_1.cpp
-//  Мазур Віталіни
+// <Мазур Віталіни>
 // Лабораторна робота № 3.1
 // Розгалуження, задане формулою: функція однієї змінної.
 // Варіант 19
+
 #include <iostream>
 #include <cmath>
 using namespace std;
 int main()
 {
-	double x; // вхідний параметр
-	double y; // результат обчислення виразу
-	double A; // проміжний результат - функціонально стала частина виразу
-	double B; // проміжний результат - функціонально змінна частина виразу
+	double x;
+	double y;
+	double A;
+	double B; 
 	cout << "x = "; cin >> x;
 
-	A = x * x;
+	A = 2 * x - 13.5;
 	// спосіб 1: розгалуження в скороченій формі
-	if (x < 0)
-		B = sin(x);
-	if (0 <= x && x <= 1)
-		B = exp(x);
+	if (x < -1)
+		B = sin(x) / (1 + cos(x * x));
+	if (-1 <= x && x <= 1)
+		B = pow(cos(x), 2) * pow(sin(x), 2) - 1;
 	if (x > 1)
-		B = cos(x);
+		B = log10(x + 0.4);
 	y = A + B;
+
 	cout << endl;
 	cout << "1) y = " << y << endl;
+
 	// спосіб 2: розгалуження в повній формі
-	if (x < 0)
-		B = sin(x);
+	if (x < -1)
+		B = sin(x) / (1 + cos(x * x));
 	else
-		if (x > 1)
-			B = cos(x);
+		if (-1 <= x && x <= 1)
+			B = pow(cos(x), 2) * pow(sin(x), 2) - 1;
 		else
-			B = exp(x);
+			B = log10(x + 0.4);
 	y = A + B;
+
+	cout << endl;
 	cout << "2) y = " << y << endl;
 	cin.get();
 	return 0;
